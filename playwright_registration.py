58656d8e922a46e6bca002c2b54e812ebd833dbe
Mikhail Prikhodko <prihodko@zm.team>
2025-09-14 13:53:41 +0300

    6.3 Маркировки Pytest

--- playwright_registration.py
+++ playwright_registration.py
@@ -1,5 +1,6 @@
 from playwright.sync_api import sync_playwright, expect
 
+
 with sync_playwright() as playwright:
     browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
---
+++ tests/test_markers.py
@@ -0,0 +1,56 @@
+import pytest
+
+@pytest.mark.smoke
+def test_some_case():
+    ...
+
+@pytest.mark.regression
+def test_regression_case():
+    pass
+
+
+@pytest.mark.smoke
+class TestSuite:
+    def test_case1(self):
+        pass
+
+    def test_case2(self):
+        pass
+
+
+@pytest.mark.regression
+class TestUserAuthentication:
+    @pytest.mark.smoke
+    def test_login(self):
+        pass
+
+    @pytest.mark.slow
+    def test_password_reset(self):
+        pass
+
+    def test_logout(self):
+        pass
+
+
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.critical
+def test_critical_login():
+    pass
+
+
+@pytest.mark.ui
+class TestUserInterface:
+
+    @pytest.mark.smoke
+    @pytest.mark.critical
+    def test_login_button(self):
+        pass
+
+    @pytest.mark.regression
+    def test_forgot_password_link(self):
+        pass
+
+    @pytest.mark.smoke
+    def test_signup_form(self):
+        pass
