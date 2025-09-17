d678690b415d30edb5a082f3d987edc03397947f
Mikhail Prikhodko <prihodko@zm.team>
2025-09-17 23:33:45 +0300

    6.5 Фикстуры Pytest. Практика работы с pytest фикстурами

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from playwright.sync_api import Playwright, Page
+from playwright.sync_api import Playwright, Page, expect
 
 
 @pytest.fixture
@@ -7,3 +7,38 @@ def chromium_page(playwright: Playwright) -> Page: # type: ignore
     browser = playwright.chromium.launch(headless=False)
     yield browser.new_page() # type: ignore
     browser.close()
+
+
+@pytest.fixture(scope='session')
+def initialize_browser_state(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration')
+
+    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
+    email_input.fill('user.name@gmail.com')
+
+    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
+    username_input.fill('username')
+
+    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
+    password_input.fill('password')
+
+    registration_btn = page.get_by_test_id('registration-page-registration-button')
+    expect(registration_btn).to_be_enabled()
+    registration_btn.click()
+
+    dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+    expect(dashboard_title).to_have_text('Dashboard')
+
+    context.storage_state(path='browser-state.json')
+
+
+@pytest.fixture(scope='function')
+def chromium_page_with_state(initialize_browser_state, playwright: Playwright) -> Page: # type: ignore
+    browser = playwright.chromium.launch(headless=False)
+    context = browser.new_context(storage_state='browser-state.json')
+    page = context.new_page()
+    yield page # type: ignore
