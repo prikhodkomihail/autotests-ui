f3c000b5c3545dc7cd0d4d6d7ddb97a4db35b869
Mikhail Prikhodko <prihodko@zm.team>
2025-09-29 22:13:48 +0300

    7.1 Знакомство с PageObject

--- tests/test_authorization.py
+++ tests/test_authorization.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import expect, Page
+from pages.login_page import LoginPage
 import pytest
 
 
@@ -13,20 +13,9 @@ import pytest
     ],
 )
 def test_wrong_email_or_password_authorization(
-    chromium_page: Page, email: str, password: str
+    login_page: LoginPage, email: str, password: str
 ):
-    chromium_page.goto(
-        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login"
-    )
-    email_inpuit = chromium_page.get_by_test_id("login-form-email-input").locator("input")
-    email_inpuit.fill(email)
-
-    password_input = chromium_page.get_by_test_id("login-form-password-input").locator("input")
-    password_input.fill(password)
-
-    login_btn = chromium_page.get_by_test_id("login-page-login-button")
-    login_btn.click()
-
-    alert = chromium_page.get_by_test_id("login-page-wrong-email-or-password-alert")
-    expect(alert).to_be_visible()
-    expect(alert).to_have_text("Wrong email or password")
+    login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+    login_page.fill_login_form(email=email, password=password)
+    login_page.click_login_btn()
+    login_page.check_visible_wrong_email_or_password_alert()
