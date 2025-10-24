4ac09c16a9ba837eed7fb0aaf261a325f0141115
prikhodkomihail <prih1202@yandex.ru>
2025-10-24 11:49:40 +0900

    7.2 Практикуемся в реализации PageObject.Практикуемся в написании автотестов с использованием POM

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import Page, expect
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 import pytest
 
 
@@ -20,3 +22,45 @@ def test_empty_courses_list(chromium_page_with_state: Page):
     expect(courses_empty_description_text).to_have_text(
             'Results from the load test pipeline will be displayed here'
         )
+
+
+
+@pytest.mark.regression
+@pytest.mark.courses
+def test_create_course(courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+    
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    
+    create_course_page.check_visible_image_preview_empty_view()
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
+    create_course_page.check_visible_create_course_form(
+        title='', estimated_time='', description='', max_score='0', min_score='0'
+    )
+
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+
+    create_course_page.upload_preview_image('./testdata/files/image.png')
+    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
+
+    create_course_page.fill_create_course_form(
+        title='Playwright',
+        estimated_time='2 weeks',
+        description='Playwright',
+        max_score='100',
+        min_score='10'
+    )
+    create_course_page.click_create_course_button()
+
+    courses_list_page.check_visible_courses_title()
+    courses_list_page.check_visible_create_course_button()
+    courses_list_page.check_visible_course_card(
+        index=0,
+        title='Playwright',
+        max_score='100',
+        min_score='10',
+        estimated_time='2 weeks'
+    )
