e8704d9dfac2428b98bcf6c4b9edf7b69ab052fb
prikhodkomihail <prih1202@yandex.ru>
2025-11-03 14:30:00 +0900

    7.6 Практическое применение PageFactory в автотестах

--- elements/file_input.py
+++ elements/file_input.py
@@ -2,6 +2,6 @@ from elements.base_element import BaseElement
 from playwright.sync_api import expect, Locator
 
 class FileInput(BaseElement):
-    def set_input_files(self, files: str, **kwargs):
-        locator = self.get_locator(**kwargs)
+    def set_input_files(self, files: str, nth: int = 0, **kwargs):
+        locator = self.get_locator(nth, **kwargs)
         locator.set_input_files(files)
