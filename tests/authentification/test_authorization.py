4b343365991177a772265818ff121d1d6e992837
prikhodkomihail <prih1202@yandex.ru>
2025-11-07 01:15:57 +0900

    8.2 Интеграция Allure в автотесты

--- tests/authentification/test_authorization.py
+++ tests/authentification/test_authorization.py
@@ -1,12 +1,25 @@
+import allure
+import pytest
+from allure_commons.types import Severity
 from pages.authentification.login_page import LoginPage
 from pages.authentification.registration_page import RegistrationPage
 from pages.dashboard.dashboard_page import DashboardPage
-import pytest
+from tools.allure.epics import AllureEpic
+from tools.allure.features import AllureFeature
+from tools.allure.stories import AllureStory
+from tools.allure.tags import AllureTag
 
 
 @pytest.mark.regression
 @pytest.mark.authorization
+@allure.tag(AllureTag.REGISTRATION, AllureTag.AUTHORIZATION)
+@allure.epic(AllureEpic.LMS)
+@allure.feature(AllureFeature.AUTHENTICATION)
+@allure.story(AllureStory.AUTHORIZATION)
 class TestAuthorization:
+    @allure.title("User login with correct email and password")
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.severity(Severity.BLOCKER)
     def test_successful_authorization(
             self,
             login_page: LoginPage,
@@ -42,6 +55,9 @@ class TestAuthorization:
             ("  ", "password"),
         ],
     )
+    @allure.title("User login with wrong email or password")
+    @allure.tag(AllureTag.USER_LOGIN)
+    @allure.severity(Severity.CRITICAL)
     def test_wrong_email_or_password_authorization(
         self, login_page: LoginPage, email: str, password: str
     ):
@@ -50,6 +66,9 @@ class TestAuthorization:
         login_page.click_login_button()
         login_page.check_visible_wrong_email_or_password_alert()
 
+    @allure.title("Navigation from login page to registration page")
+    @allure.tag(AllureTag.NAVIGATION)
+    @allure.severity(Severity.NORMAL)
     def test_navigate_from_authorization_to_registration(
             self,
             login_page: LoginPage,
